36fce0efd4292b40c3f0f46d979cebed902b3cf5
anindyoo <anindyo0216@gmail.com>
2024-07-27 15:49:04 +0700

    feat: add the header component.

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,7 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
 
 const Main = () => (
-  <Outlet />
+  <>
+    <Header />
+    <Outlet />
+  </>
 );
 
 export default Main;
