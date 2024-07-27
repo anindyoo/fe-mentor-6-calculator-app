02460d5909ffb9b9a32127930d987929be8ec843
anindyoo <anindyo0216@gmail.com>
2024-07-27 15:43:46 +0700

    feat: implement theme colors.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layouts/Main';
 import Home from './pages/Home';
+import useThemeStore from './store/themeStore';
 
 const App = () => {
   const routes = [
@@ -21,11 +22,17 @@ const App = () => {
 
   const router = createBrowserRouter(routes, routeConfig);
 
+  const {
+    activeTheme,
+  } = useThemeStore();
+
   return (
     <div className={`
     APP-CONTAINER
+    flex flex-col justify-center
     h-screen w-screen
-    `}
+    ${activeTheme.text.general}
+    ${activeTheme.bg.page}`}
     >
       <RouterProvider router={router} />
     </div>
