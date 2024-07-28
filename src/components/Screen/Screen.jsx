dc534238105c89321756a429ef4470691f404899
anindyoo <anindyo0216@gmail.com>
2024-07-28 20:30:29 +0700

    feat: update screen text's letter spacing.

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -39,7 +39,7 @@ const Screen = () => {
       flex flex-row justify-end
       max-w-[29.875rem]
       text-[3.5rem] text-right font-bold
-      text-nowrap overflow-hidden tracking-[-0.02em]"
+      text-nowrap overflow-hidden tracking-[0.01em]"
         >
           {equationDisplay}
         </div>
