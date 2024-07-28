aacf86ac78a87ae4a8a4f8d3dc256effada17e69
anindyoo <anindyo0216@gmail.com>
2024-07-28 20:24:15 +0700

    feat: create store hook for calculator state management & add method to set the equation input string.

---
+++ src/store/calculatorStore.jsx
@@ -0,0 +1,60 @@
+import { create } from 'zustand';
+
+const useCalculatorStore = create((set) => ({
+  equationDisplay: '0',
+  alreadyDecimal: false,
+  alreadyOperator: false,
+  setEquationInput: (input) => {
+    set((state) => {
+      const stateObject = {
+        equationDisplay: state.equationDisplay,
+        alreadyDecimal: state.alreadyDecimal,
+        alreadyOperator: state.alreadyOperator,
+      };
+
+      const lastInput = stateObject.equationDisplay.slice(-1);
+
+      switch (input) {
+        case '.':
+          if (stateObject.alreadyDecimal === false) {
+            const isNotaString = Number.isNaN(Number(lastInput));
+            stateObject.equationDisplay = isNotaString
+              ? `${stateObject.equationDisplay}0.`
+              : stateObject.equationDisplay + input;
+            stateObject.alreadyOperator = true;
+          }
+          stateObject.alreadyDecimal = true;
+          break;
+        case '-':
+          stateObject.equationDisplay = (lastInput === '-' || lastInput === '+' || lastInput === '.')
+            ? stateObject.equationDisplay.slice(0, -1) + input
+            : stateObject.equationDisplay + input;
+          stateObject.alreadyOperator = true;
+          stateObject.alreadyDecimal = false;
+          break;
+        case '+':
+        case '/':
+        case 'x':
+          if (lastInput === '-' && !(/\d/.test(stateObject.equationDisplay.slice(-2)))) {
+            break;
+          } else {
+            stateObject.equationDisplay = stateObject.alreadyOperator === false
+              ? stateObject.equationDisplay + input
+              : stateObject.equationDisplay.slice(0, -1) + input;
+            stateObject.alreadyOperator = true;
+            stateObject.alreadyDecimal = false;
+          }
+          break;
+        default:
+          stateObject.equationDisplay = stateObject.equationDisplay === '0'
+            ? input
+            : stateObject.equationDisplay + input;
+          stateObject.alreadyOperator = false;
+          break;
+      }
+      return stateObject;
+    });
+  },
+}));
+
+export default useCalculatorStore;
