a260f3ca33e39a7aa91fe6782d121cee8d8f4ee1
anindyoo <anindyo0216@gmail.com>
2024-07-29 12:21:34 +0700

    feat: remove pre elements.

--- src/components/Screen/Screen.jsx
+++ src/components/Screen/Screen.jsx
@@ -8,43 +8,28 @@ const Screen = () => {
 
   const {
     equationDisplay,
-    alreadyDecimal,
-    alreadyOperator,
   } = useCalculatorStore();
 
   return (
-    <>
-      <pre>
-        alreadyDecimal:
-        {JSON.stringify(alreadyDecimal, null, 1)}
-      </pre>
-      <pre>
-        alreadyOperator:
-        {JSON.stringify(alreadyOperator, null, 1)}
-      </pre>
-      <pre>
-        {JSON.stringify(equationDisplay, null, 1)}
-      </pre>
-      <div className={`
-    SCREEN-COMPONENT
-    SECTION-CENTER
-    h-32
-    px-8
-    content-center
-    rounded-lg
-    ${activeTheme.bg.screen}`}
+    <div className={`
+      SCREEN-COMPONENT
+      SECTION-CENTER
+      h-32
+      px-8
+      content-center
+      rounded-lg
+      ${activeTheme.bg.screen}`}
+    >
+      <div className="
+        SCREEN-TEXT
+        flex flex-row justify-end
+        max-w-[29.875rem]
+        text-[3.5rem] text-right font-bold
+        text-nowrap overflow-hidden tracking-[0.01em]"
       >
-        <div className="
-      SCREEN-TEXT
-      flex flex-row justify-end
-      max-w-[29.875rem]
-      text-[3.5rem] text-right font-bold
-      text-nowrap overflow-hidden tracking-[0.01em]"
-        >
-          {equationDisplay}
-        </div>
+        {equationDisplay.toLocaleString('en-US')}
       </div>
-    </>
+    </div>
   );
 };
 
